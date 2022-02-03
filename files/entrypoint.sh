d33d6a278a226110528b90fe3c418a26a2a245ec
Mario Melcher <mario.melcher@seitenbau.com>
2022-02-03 08:28:07 +0100

    add win and mac support using caddy reverse-proxy

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -e
 
+if [ "$REVERSEPROXY_ENABLED" == "true" ]
+then
+  # Start caddy reverse-proxy
+  caddy reverse-proxy --from :24730 --to 127.0.0.1:24727 &
+fi
+
 if [ "$XVFB_ENABLED" == "true" ]
 then
   # start X Window Virtual Framebuffer
